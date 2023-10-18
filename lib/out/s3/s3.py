107fc3086474336d45c44463aa24086933687c25
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-10-18 18:19:45 +0900

    [feat] done

---
+++ lib/out/s3/s3.py
@@ -0,0 +1,34 @@
+import json
+import logging
+import os
+from typing import Any, Mapping, Sequence
+
+import boto3
+from boto3_type_annotations.s3 import Client
+
+
+class S3Sender:
+    def __init__(self, *args, **kwargs):
+        self.logger = logging.getLogger("scrapy.sender")
+
+    def send(
+        self, rel_name: str, data: Sequence[Mapping[str, Any]], *args, **kwargs
+    ) -> bool:
+        """
+        Data를 100개씩 쪼개 {rel_name}_{idx}.json으로 전송
+        """
+        try:
+            s3: Client = boto3.client("s3")
+            for idx, p in enumerate(range(0, len(data), 100)):
+                buffer = data[p : p + 100]
+                body: bytes = json.dumps(buffer, ensure_ascii=False).encode()
+                s3.put_object(
+                    Bucket=os.getenv("S3_BUCKET"),
+                    Key=f"{os.getenv('S3_KEY')}/{rel_name}_{idx}.json",
+                    Body=body,
+                )
+        except Exception as e:
+            self.logger.error(e)
+            return False
+        else:
+            return True
