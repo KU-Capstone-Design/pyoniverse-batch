12b2f4645d2f30800d14f67f7ebc5030109b2e96
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-10-30 17:22:27 +0900

    [fix] split db update

--- lib/out/sender/db/db.py
+++ lib/out/sender/db/db.py
@@ -2,6 +2,7 @@ import json
 import os
 from dataclasses import asdict
 from datetime import datetime
+from time import sleep
 from typing import NoReturn, Sequence
 
 import boto3
@@ -23,16 +24,19 @@ class DBSender:
             sqs_queue_url: str = sqs_client.get_queue_url(
                 QueueName=os.getenv("DB_QUEUE_NAME")
             )["QueueUrl"]
-            message = Message(
-                date=datetime.strftime(date, "%Y-%m-%d"),
-                db_name=db_name,
-                rel_name=rel_name,
-                origin="transform",
-                data=data,
-            )
-            sqs_client.send_message(
-                QueueUrl=sqs_queue_url,
-                MessageBody=json.dumps(asdict(message)),
-            )
+            for p in range(0, len(data), 10):
+                # 나눠서 보내기
+                message = Message(
+                    date=datetime.strftime(date, "%Y-%m-%d"),
+                    db_name=db_name,
+                    rel_name=rel_name,
+                    origin="transform",
+                    data=data[p : p + 10],
+                )
+                sqs_client.send_message(
+                    QueueUrl=sqs_queue_url,
+                    MessageBody=json.dumps(asdict(message)),
+                )
+                sleep(10)
         except Exception as e:
             raise e
