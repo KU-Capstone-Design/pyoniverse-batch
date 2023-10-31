b2c1adda5e02ffa48b917c219730cde7d3a96c3f
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-10-31 22:03:47 +0900

    [fix] test

--- tests/out/test_finish.py
+++ tests/out/test_finish.py
@@ -20,6 +20,7 @@ def test_send_finish_event(env):
     sender = EventSender()
     # when & then
     try:
-        assert sender.send(event_type="finished", date=datetime.utcnow()) is True
+        sender.send(event_type="finished", date=datetime.utcnow())
+        assert True
     except Exception:
         assert False
