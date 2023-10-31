db3ab344473a5d902a0523ab42fc2f044041ad7d
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-10-31 20:25:47 +0900

    [feat] resource injector

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser
 
 import dotenv
 
+from lib.dependency_injector.resource import ResourceInjector
 from lib.engine import Engine
 
 
@@ -17,15 +18,13 @@ parser.add_argument("--stage", choices=["dev", "test", "prod"])
 parser.add_argument("--date", help="실행된 날짜 정보. yyyy-MM-dd 형식 ex) 2023-11-11")
 if __name__ == "__main__":
     args = parser.parse_args()
-    logger = logging.getLogger("batch")
-    logger.setLevel(logging.ERROR)
+    resource_injector = ResourceInjector()
+    resource_injector.init_resources()
+    logger = logging.getLogger(__name__)
     try:
         engine = Engine(stage=args.stage, date=args.date)
         res = engine.run()
-        if res:
-            exit(0)  # 정상 종료
-        else:
-            exit(1)  # 비정상 종료
+        exit(0)
     except Exception as e:
         logger.error(e)
         exit(1)  # 비정상 종료
